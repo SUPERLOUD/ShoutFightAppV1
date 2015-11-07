1a7404899112af575d922f7b746aa9df585f38cd
root <brian_chuk@yahoo.com>
2015-11-07 15:30:42 -0500

    single button communication successful! :fire::fire::fire:

--- ShoutFightAppV1/app/src/main/java/testapps/shoutfight/shoutfightappv1/MainActivity.java
+++ ShoutFightAppV1/app/src/main/java/testapps/shoutfight/shoutfightappv1/MainActivity.java
@@ -2,15 +2,38 @@ package testapps.shoutfight.shoutfightappv1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity {
+
+    RequestQueue queue;
+    // Tag used to log messages
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
+
+        TextView textView = (TextView) findViewById(R.id.ayy);
+        textView.setText("ayy");
     }
 
     @Override
@@ -33,5 +56,41 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        StringRequest sr = new StringRequest(Request.Method.POST,"https://api.syncano.io/v1/instances/dark-snowflake-7198/webhooks/action/run/",
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            response,
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(TAG, error.toString(), error);
+                }
+            }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<>();
+                params.put("id","1");
+                params.put("dummy","1000");
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("X-API-KEY","7376106916d5ae8ab578040ae303e860f76c0b50");
+                return params;
+            }
+            };
+    queue.add(sr);
     }
 }
